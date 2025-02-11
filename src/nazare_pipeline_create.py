61cae713753b17b88ba8eb4cb219a1c4dcfe4d41
Taehwan Kim <ohpowel@gmail.com>
2025-02-11 14:47:42 +0900

    Download S3 file schema if possible

--- src/nazare_pipeline_create.py
+++ src/nazare_pipeline_create.py
@@ -4,7 +4,7 @@ import argparse
 import logging
 
 from utils.nazare import pipeline_create
-from utils.utils import load_rows
+from utils.utils import download_s3file, load_rows
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -21,6 +21,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "--store-api-password", help="Store API password", required=True
     )
+
+    # File
+    parser.add_argument(
+        "--s3-endpoint",
+        help="S3 url",
+        default="http://rook-ceph-rgw-ceph-objectstore.rook-ceph.svc.cluster.local:80",
+    )
+    parser.add_argument("--s3-accesskey", help="S3 accesskey")
+    parser.add_argument("--s3-secretkey", help="S3 secretkey")
 
     parser.add_argument("--pipeline-name", help="Pipeline name", required=True)
 
@@ -54,12 +63,18 @@ if __name__ == "__main__":
         format="%(asctime)s %(levelname)-8s %(name)-12s: %(message)s",
     )
 
+    schema_file = args.schema_file
+    if schema_file.startswith("s3a://"):
+        schema_file = download_s3file(
+            schema_file, args.s3_accesskey, args.s3_secretkey, args.s3_endpoint
+        )
+
     pipeline_create(
         args.store_api_url,
         args.store_api_username,
         args.store_api_password,
         args.pipeline_name,
-        load_rows(args.schema_file, args.schema_file_type),
+        load_rows(schema_file, args.schema_file_type),
         args.pipeline_deltasync_enabled,
         args.pipeline_retention,
         logger=logging,
